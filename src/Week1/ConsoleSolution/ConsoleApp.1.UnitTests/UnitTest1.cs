2c16663c9ab271837fd178aba0ad3983d84142d2
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-18 15:58:17 -0400

    Ship the API!

---
+++ src/Week1/ConsoleSolution/ConsoleApp.1.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp._1.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+string message = "Welcome to Class";
+
+DateTime now = DateTime.Now;
+
+string finalMessage = "$The Message {message} and it is now {now:T}";
+
+var app = builder.Build();
+
+app.MapGet("/message", () =>
+{
+    var response = new MessageResponseModel("This is an API! Wow!", DateTime.Now);
+    return Results.Ok(finalMessage);
+});
+
+app.Run();
+
+public record MessageResponseModel(string Message, DateTime When);
