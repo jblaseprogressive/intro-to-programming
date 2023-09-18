8973f058ba9b47f329191f2eeafde21198da26c5
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-18 16:31:55 -0400

    changes

--- src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
+++ src/Week1/ConsoleSolution/ConsoleApp1/Program.cs
@@ -1,19 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-string message = "Welcome to Class";
-
-DateTime now = DateTime.Now;
-
-string finalMessage = "$The Message {message} and it is now {now:T}";
 
 var app = builder.Build();
 
 app.MapGet("/message", () =>
 {
     var response = new MessageResponseModel("This is an API! Wow!", DateTime.Now);
-    return Results.Ok(finalMessage);
+    return Results.Ok(response);
 });
 
+
 app.Run();
+
 
 public record MessageResponseModel(string Message, DateTime When);
