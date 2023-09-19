30151a6edd9a910cb8384b65d85feddf35208c21
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-19 15:11:38 -0400

    more change stuff

--- src/Week1/BankingSolution/Banking.Domain/Account.cs
+++ src/Week1/BankingSolution/Banking.Domain/Account.cs
@@ -2,16 +2,27 @@
 
 public class Account
 {
-
     private decimal _balance = 5000M;
     public void Deposit(decimal amountToDeposit)
     {
-        _balance = _balance + amountToDeposit;
+        _balance += amountToDeposit;
     }
 
     public decimal GetBalance()
     {
 
         return _balance;
+    }
+
+    public void Withdraw(decimal amountToWithdraw)
+    {
+        if (amountToWithdraw <= _balance)
+        {
+            _balance -= amountToWithdraw;
+        }
+        else
+        {
+            throw new OverdraftException();
+        }
     }
 }
---
+++ src/Week1/BankingSolution/Banking.UnitTests/BankAccount/MakingWithdraws.cs
@@ -0,0 +1,35 @@
+﻿using Banking.Domain;
+
+namespace Banking.UnitTests.BankAccount;
+public class MakingWithdraws
+{
+    [Theory]
+    [InlineData(82.23)]
+    [InlineData(200)]
+    public void MakingAWithdrawalDecreasesTheBalance(decimal amountToWithdraw)
+
+    {
+        // Given
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+
+        // When
+        account.Withdraw(amountToWithdraw);
+
+
+        //then
+        Assert.Equal(openingBalance - amountToWithdraw, account.GetBalance());
+    }
+
+
+
+    [Fact]
+    public void CanTakeEntireBalance()
+    {
+        var account = new Account();
+
+        account.Withdraw(account.GetBalance());
+
+        Assert.Equal(0, account.GetBalance());
+    }
+}
