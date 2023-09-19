11dff0913d34c8230a02872ba56a1317ffaf0167
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-19 11:15:39 -0400

    created banking stuff

--- src/CSharpNotesSolution/CSharpNotes/Types/DeclaringLocalVariables.cs
+++ src/CSharpNotesSolution/CSharpNotes/Types/DeclaringLocalVariables.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpNotes.Types;
-internal class DeclaringLocalVariables
+﻿namespace CSharpNotes.Types;
+public class DeclaringLocalVariables
 {
+    [Fact]
+    public void ExplicitlyTypedLocalVariable()
+    {
+        // Type identifier [= value]
+        int a = 0;
+        int b = 2;
+
+        string dogName = "Rover";
+
+        Assert.Equal("Rover", dogName);
+
+        Assert.Equal(0, a);
+        Assert.Equal(2, b);
+
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariablesWithVar()
+    {
+        var a = 0;
+        var b = 1.0;
+        var c = "Cat";
+        var d = 'A';
+        var e = true;
+        var salary = 80123.23M; //adding M makes it decimal!
+
+        var rover = new Dog();
+    }
+
+    [Fact]
+    public void ImplicitlyTypedlocalVariablesWithNew()
+    {
+        // .NET 5
+        Dog rover = new();
+    }
 }
+
+public class Dog { }
