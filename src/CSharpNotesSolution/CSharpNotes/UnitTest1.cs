299b2cca4b939639a7d36da248863d6c4c9b88ee
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-19 10:28:21 -0400

    CSharpNotes Started

---
+++ src/CSharpNotesSolution/CSharpNotes/UnitTest1.cs
@@ -0,0 +1,29 @@
+namespace CSharpNotes;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CanAddTwoSpecificIntegers()
+    {
+        //Given
+        int a = 10;
+        int b = 20;
+        int sum;
+
+        // When
+        sum = a + b;
+
+        Assert.Equal(30, sum);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(2, 2, 4)]
+    [InlineData(10, 3, 13)]
+    public void GivenAnyTwoIntegersWeCanAddThemTogether(int a, int b, int expected)
+    {
+        int sum = a + b;
+        Assert.Equal(expected, sum);
+    }
+
+}
