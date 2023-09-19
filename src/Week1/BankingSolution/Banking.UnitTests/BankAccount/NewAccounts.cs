58ebf515e0fb8321e9263167baee5a7cb4b23939
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-19 12:15:01 -0400

    can get balance

---
+++ src/Week1/BankingSolution/Banking.UnitTests/BankAccount/NewAccounts.cs
@@ -0,0 +1,19 @@
+﻿using Banking.Domain;
+
+namespace Banking.UnitTests.BankAccount;
+public class NewAccounts
+{
+    [Fact]
+    public void NewAccountsHavetheCorrectOpeningBalance()
+    {
+        //write the code we wish we had
+        // given
+        var account = new Account();
+
+
+        //when
+        decimal balance = account.GetBalance();
+        //then
+        Assert.Equal(5000M, balance);
+    }
+}
