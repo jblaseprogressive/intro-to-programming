23a89bb08e9fc3e489872f21f68e3392daf78d52
jblaseprogressive <jacob_c_blase@progressive.com>
2023-09-20 10:44:20 -0400

    string calc stuff

---
+++ src/Week1/StringCalculator-Weds-9-20/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+        else
+        {
+            string[] numbersArray = numbers.Split(',');
+            int sum = 0;
+            foreach (string num in numbersArray)
+            {
+                sum += int.Parse(num);
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ src/Week1/StringCalculator-Weds-9-20/StringCalculatorTests.cs
@@ -0,0 +1,29 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("");
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SingleNumberReturnsItself()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1");
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void TwoNumbersSeparatedByComma()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2");
+        Assert.Equal(3, result);
+    }
+}
